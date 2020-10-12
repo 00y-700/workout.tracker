8cf81c40e9931111c4fe9d9c532c77eea9b24158
Jeffrey Harding <jeffreyh202@gmail.com>
2020-10-12 11:44:32 -0600

    mongo set up

--- server.js
+++ server.js
@@ -13,14 +13,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
 app.use(require("./routes/api.js"));
-app.use(require("./routes/view.js"));
+app.use(require("./routes/html.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
